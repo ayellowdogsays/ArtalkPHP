21b87ab259df8679b1a02c4a1da6291166d1bd05
阿黄说 <ahuangsays@outlook.com>
2025-05-18 18:47:09 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
-phpinfo();
+use app\ArtalkServer;
+
+define('CONFIG_FILE_PATH', __DIR__ . '/../Config.php');
+define('LAZER_DATA_PATH', __DIR__ . '/../data/');
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../app/ArtalkServer.php');
+
+new ArtalkServer();
